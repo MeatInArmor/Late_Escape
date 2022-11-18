b06f7eb5d1688144aa769d2fde0386a74513584f
MeatInArmor <reinor.1st@gmail.com>
2022-11-19 01:09:23 +0300

    Max
    Переработка статичного класса для персонажей. теперь он рабочий (вроде)

--- Assets/Scripts/ObjectClasses/CharacterValues.cs
+++ Assets/Scripts/ObjectClasses/CharacterValues.cs
@@ -10,25 +10,24 @@ public class CharacterValues : MonoBehaviour
     public GameObject[] _characterModel;
 
  // Структура персоанжей с характеристиками
-    public static List<Character> character = new List<Character>();
+    public static Character[] character = new Character[4]; // 0 - фермер; 1 - дочь; 2 - урядник; 3 - воин
     public struct Character
     {
-        public int currentLevel;
-        public int levelPoints;
-        public int maxHP;
-        public int currentHP;
-        //public int maxStamina;
-        //public int currentStamina;
-        //public int maxMana;
-        //public int currentMana;
-        public float magicDammageMultiplier;
-        public float chargeAttackDammageMultiplier;
-        public int dammage;
-        public int deffence;
-        public bool isDefeated;
-        public float moveSpeedMultiplier;
-
-    }
+        //public int _currentLevel; //уровень персонажа
+        //public int _levelPoints;  //опыт персонажа для увеличения уровня
+        public int _maxHP; // максимальное здоровье
+        public int _currentHP; // текущее здоровье
+        //public int _maxStamina; // максимальная выносливость
+        //public int _currentStamina; // текущая выносливость
+        //public int _maxMana; // максимальная мана
+        //public int _currentMana; //текущая мана
+        public float _magicDammageMultiplier; // множитель урона магии (пока что от обычного урона)
+        //public float _chargeAttackDammageMultiplier; // множитель урона сильной атаки
+        public int _damage; // урон (средний)
+        public int _deffence; // блокируемый урон (в единицах)
+        public bool _isDefeated; // персонаж побеждён
+        public float _moveSpeedMultiplier; //множитель скорости передвижения
+    }  
 
     // игровые аспекты
     public static bool canMove; // становится false во время атаки, сильной атаки, магии, переключения персонажа, поражения 
@@ -39,20 +38,37 @@ public class CharacterValues : MonoBehaviour
     public static int currentTeamMember; // номер текущего члена отряда
     public static float teamMemberChangeCD; // время перезарядки смены персонажа
     public static float teamMemberChangeCDDeltaTime; // время до следующей смены персонажа
-    public static bool fightMode; // находится ли персонаж в состоянии боя
+    public static bool inFightMode; // находится ли персонаж в состоянии боя
     
     // Взаимодействия со врагами
-    public static List<Enemy> enemyInFight = new List<Enemy>(); // список врагов, с которыми сражаемся в данный момент
+    public static List<Enemy> enemyInFight = new List<Enemy>(); // список врагов, с которыми сражаемся в данный момент 
+    //public static List<GameObject> enemyInFight = new List<GameObject>(); 
     public static Enemy enemyCurrentTarget; // текущая цель атаки
+    //public static GameObject enemyCurrentTarget;
     public static float distanceToEnemyCurrentTarget; //дистанция до цели атаки
-
 
 private void Awake()
 {
+    canMove = true;
+    isCameraRotationBlocked = false;
+
     teamMembers = 1;
     currentTeamMember = 1;
-    //character[0].maxHP = 20;
-    //Debug.Log(character[0].maxHP);
+    teamMemberChangeCD = 5;
+    teamMemberChangeCDDeltaTime = 0;
+    inFightMode = false;
+
+    CharacterValues.character[0]._maxHP = 40;
+    CharacterValues.character[0]._currentHP = 40;
+    CharacterValues.character[0]._magicDammageMultiplier = 1.4f;
+    CharacterValues.character[0]._damage = 8;
+    CharacterValues.character[0]._deffence = 2;
+    CharacterValues.character[0]._isDefeated = false;
+    CharacterValues.character[0]._moveSpeedMultiplier = 1;
+
+
+
+    Debug.Log(CharacterValues.character[0]._maxHP);
 
 }
 }
